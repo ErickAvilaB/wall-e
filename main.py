f716b36f3463eb099b1514e54c887e22168b7c5c
AvilaErick <321159063@ciencias.unam.mx>
2024-11-12 19:41:05 -0600

    Se eliminan comandos

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import logging
-import os
+from os import getenv
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
 load_dotenv()
 
-api_token: str = os.getenv('API_TOKEN')
+api_token: str = getenv('API_TOKEN')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -18,11 +18,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
 def main() -> None:
     application = ApplicationBuilder().token(api_token).build()
 
     start_handler = CommandHandler('start', start)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
     application.add_handler(start_handler)
+    application.add_handler(unknown_handler)
 
     application.run_polling()
 
