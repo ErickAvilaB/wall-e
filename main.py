0431da4281a14b10b312018c009eca3db0554f25
ErickAvilaB <yahir7083@gmail.com>
2024-11-16 21:22:59 -0600

    agrega documentacion a main

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from src import ExpresionEcho, ExpresionDesconocido, ExpresionAyuda, ExpresionAc
 
 
 def configurar_logging() -> None:
+    """
+    Imprime los mensajes de log en la consola. En formato: [fecha] - [nombre] - [nivel] - [mensaje]
+    """
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
@@ -13,10 +16,22 @@ def configurar_logging() -> None:
 
 
 def crea_bot(api_token: str) -> ApplicationBuilder:
+    """
+    Crea un bot de Telegram con el token proporcionado.
+    Args:
+        api_token (str): Token de la API de Telegram.
+    Returns:
+        ApplicationBuilder: Bot de Telegram.
+    """
     return ApplicationBuilder().token(api_token).build()
 
 
-def analizar_comandos() -> None:
+def crea_manejador() -> list:
+    """
+    Analiza los comandos proporcionados por el usuario.
+    Returns:
+        list: Lista de manejadores de comandos.
+    """
     ayuda_handler = CommandHandler('ayuda', ExpresionAyuda().interpretar)
     start_handler = CommandHandler('start', ExpresionAyuda().interpretar)
     accion_handler = CommandHandler('accion', ExpresionAccion().interpretar)
@@ -29,6 +44,9 @@ def analizar_comandos() -> None:
 
 
 def main() -> None:
+    """
+    Define el punto de entrada de la aplicación.
+    """
     configurar_logging()
 
     load_dotenv()
@@ -37,7 +55,7 @@ def main() -> None:
 
     application = crea_bot(api_token)
 
-    for handler in analizar_comandos():
+    for handler in crea_manejador():
         application.add_handler(handler)
 
     application.run_polling()
